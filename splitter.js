64d7767fa5050963272051590ad90ca2639aa106
toremann <50668920+toremann@users.noreply.github.com>
2022-05-09 08:23:24 +0200

    prettier

--- splitter.js
+++ splitter.js
@@ -1,34 +1,45 @@
-const fs = require('fs');
-const fileName = 'data/data.json';
-const dir = 'newData/'
+const fs = require("fs");
+const fileName = "data/data.json";
+const dir = "newData/";
 
-// read the file
-fs.readFile(fileName, 'utf8', function (err, data) {
+// Read the file
+fs.readFile(fileName, "utf8", function (err, data) {
+  if (err) {
+    console.log("Readfile error", err);
+  }
 
-    var newData = JSON.parse(data)
-    var stockSymbol = [];
+  var newData = JSON.parse(data);
+  var stockSymbol = [];
 
-    console.log(newData[1])
+  // Push the symbols for creating JSON files
+  for (const [key, value] of Object.entries(newData)) {
+    stockSymbol.push(value.instrument_info.symbol);
+  }
 
-    // Push the symbols for creating JSON files
-    for (const [key, value] of Object.entries(newData)) {
+  for (let i = 0; i < stockSymbol.length; i++) {
+    let stockName = JSON.stringify(stockSymbol[i]).replace(/(^"|\"$)/gm, "");
+    let stockData = JSON.stringify(newData[i], null, 2);
 
-        stockSymbol.push(value.instrument_info.symbol);
+    console.log(
+      "Wrote data for:",
+      stockName,
+      "|",
+      "Stocks to write:",
+      i + 1,
+      "of",
+      stockSymbol.length
+    );
 
+    fs.writeFile(dir + `${stockName}.json`, stockData, (err) => {
+      if (err) {
+        console.log("Writefile error", err);
+      }
+    });
+  }
+  fs.readdir(dir, (err, files) => {
+    if (err) {
+      console.log("Readdir error", err);
     }
-
-    for (let i = 0; i < stockSymbol.length; i++) {
-
-        let stockName = JSON.stringify(stockSymbol[i]).replace(/(^"|\"$)/mg, '')
-        let someData = JSON.stringify(newData[i], null, 2)
-
-        console.log('Wrote data for:', stockName, '|', 'Stocks to write:', i+1, 'of', stockSymbol.length)
-
-        fs.writeFile(dir + `${stockName}.json`, someData, err => {
-            if (err) {
-                console.log(err)
-            }
-        })
-    }
- })
-
+    console.log("Files created:", files.length);
+  });
+});
